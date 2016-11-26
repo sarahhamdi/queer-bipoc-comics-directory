7295ed730016ddf437e63fd6a337184e63ba1bb1
Sarah Hamdi <s.hamdi@gmail.com>
2016-11-26 01:03:40 -0500

    successful API calls

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,56 @@
-const ohHey = "Hello World";
+/*jshint esversion: 6 */
 
-console.log(ohHey);
+let sheetsu = {};
+sheetsu.endpoint = 'https://sheetsu.com/apis/v1.0/ea0629575c7c';
+let generic = {};
+generic.success = (data) => console.log(data);
+// data.filter((book) => {
+//     if (book.authorLGBTQ == 'y') {
+//         console.log(book);
+//     }
+// });
+generic.error = (data) => console.log(data);
+
+let goodreads = {};
+goodreads.endpoint = 'https://www.goodreads.com/book/title.xml';
+goodreads.key = 'AYoaCzPGXisCTWsP6Ainw';
+
+
+// +++++++++ API CALLS ++++++++++++++++++++++
+sheetsu.getData = () =>
+    $.ajax({
+        url: sheetsu.endpoint,
+        dataType: 'json',
+        type: 'GET',
+        // place for handling successful response
+        success: generic.success,
+        // handling error response
+        error: generic.error,
+    });
+
+goodreads.getData = () =>
+    $.ajax({
+        url: 'http://proxy.hackeryou.com',
+        dataType: 'json',
+        method: 'GET',
+        data: {
+            reqUrl: goodreads.endpoint,
+            params: {
+                key: goodreads.key,
+                title: "This Is How You Lose Her"
+            },
+            xmlToJSON: true
+        },
+        success: generic.success,
+        error: generic.error,
+    });
+
+
+
+// +++++++++ DOCUMENT READY ++++++++++++++++++++++
+$(function() {
+
+    sheetsu.getData();
+    goodreads.getData();
+
+});
