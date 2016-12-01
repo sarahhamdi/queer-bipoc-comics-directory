2b03ff4c872a02adf0bc5da9fc9c3f1e07899ee9
Sarah Hamdi <s.hamdi@gmail.com>
2016-11-30 19:20:56 -0500

    iterate over sheetsu data

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -14,25 +14,29 @@ goodreads.key = 'AYoaCzPGXisCTWsP6Ainw';
 generic.error = (data) => console.log(data);
 goodreads.success = (data) => console.log(data.GoodreadsResponse.book.description);
 sheetsu.success = (books) => {
-    books.filter((book) => {
-        if (book.authorLGBTQ == 'y') {
-            console.log(book.bookTitle);
-            goodreads.getData(book.bookTitle);
-        }
-    });
+    console.log(books);
+    let things = generic.userInputArray;
+    let filtered = books.filter(book =>
+        // for every user selection value
+        // this function returns an array of books 
+        // that have the 'key' value of that selection as 'y' (e.g. 'authorLGBTQ' = 'y')
+        things.every(thing =>
+            // uses bracket notation bc you can't use dot notation with arrays
+            book[thing] === 'y'));
+    console.log(filtered);
 };
 
 // +++++++++ API CALLS ++++++++++++++++++++++
-// sheetsu.getData = () =>
-//     $.ajax({
-//         url: sheetsu.endpoint,
-//         dataType: 'json',
-//         type: 'GET',
-//         // place for handling successful response
-//         success: sheetsu.success,
-//         // handling error response
-//         error: generic.error,
-//     });
+sheetsu.getData = () =>
+    $.ajax({
+        url: sheetsu.endpoint,
+        dataType: 'json',
+        type: 'GET',
+        // place for handling successful response
+        success: sheetsu.success,
+        // handling error response
+        error: generic.error,
+    });
 
 // goodreads.getData = (title) =>
 //     $.ajax({
@@ -51,80 +55,29 @@ sheetsu.success = (books) => {
 //         error: generic.error,
 //     });
 
-sheetsu.getData = [{
-        "author": "Alison Bechdel",
-        "authorPoC": "n",
-        "authorLGBTQ": "y",
-        "bookTitle": "The Essential Dykes to Watch Out For",
-        "characterLGBTQ": "y",
-        "characterPoC": "y",
-        "webcomicWithPrintBook": ""
-    },
-    {
-        "author": "Sophie Campbell",
-        "authorPoC": "y",
-        "authorLGBTQ": "y",
-        "bookTitle": "Wet Moon, Volume 1: Feeble Wanderings",
-        "characterLGBTQ": "y",
-        "characterPoC": "y",
-        "webcomicWithPrintBook": ""
-    },
-    {
-        "author": "Kelly Thompson, Sophie Campbell (artist)",
-        "authorPoC": "n",
-        "authorLGBTQ": "y",
-        "bookTitle": "Jem and the Holograms",
-        "characterLGBTQ": "n",
-        "characterPoC": "n",
-        "webcomicWithPrintBook": ""
-    },
-    {
-        "author": "Mariko Tamaki, Jillian Tamaki (illustrator)",
-        "authorPoC": "y",
-        "authorLGBTQ": "y",
-        "bookTitle": "Skim",
-        "characterLGBTQ": "y",
-        "characterPoC": "y",
-        "webcomicWithPrintBook": ""
-    },
-];
-// +++++++++ EVENT HANDLER ++++++++++++++++++++++
+// // +++++++++ EVENT HANDLER ++++++++++++++++++++++
+generic.getUserInput = () => {
 
-var things = ['authorPoC', 'authorLGBTQ'];
-
-generic.getUserInput = () =>
     $('.choose-comics').on('submit', function(event) {
-        event.preventDefault();
         generic.userInputArray = [];
+        event.preventDefault();
         $('input:checked').each(function() {
             generic.userInputArray.push($(this).val());
             console.log($(this).val());
         });
         console.log(generic.userInputArray);
-        console.log(filtered);
+        sheetsu.getData();
+        // console.log(filtered);
         // filtered(generic.userInputArray);
 
     });
-
-// filters through all the sheetsu data
-var filtered = (things) =>
-    sheetsu.getData.filter(book =>
-        // for every user selection value
-        // this function returns an array of books 
-        // that have the 'key' value of that selection as 'y' (e.g. 'authorLGBTQ' = 'y')
-        things.every(thing =>
-            // uses bracket notation bc you can't use dot notation with arrays
-            book[thing] === 'y'));
-
-
+};
 
 // +++++++++ DOCUMENT READY ++++++++++++++++++++++
 $(function() {
 
-    // sheetsu.getData();
+    sheetsu.getData();
     generic.getUserInput();
-    // console.log(filtered);
-    // filtered();
 
 });
 
